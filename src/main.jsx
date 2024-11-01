27dfa499c83d2b550970eaba38123e9a7bed5330
SumiyaRahman <sumiarahman01739@gmail.com>
2024-10-31 23:28:05 -0600

    basic structure

--- src/main.jsx
+++ src/main.jsx
@@ -5,14 +5,27 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import './index.css'
-import App from './App.jsx'
 import Root from './Components/Root/Root.jsx';
+import ErrorPage from './Components/ErrorPage/ErrorPage.jsx';
+import Home from './Components/Home/Home.jsx';
+import Dashboard from './Components/Dashboard/Dashboard.jsx';
 
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root></Root>,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>
+      },
+      {
+        path: '/dashboard',
+        element: <Dashboard></Dashboard>
+      }
+    ],
   },
 ]);
 
